228dc9fe534f6df9cf2a7b1c1a20d2058de35e81
Duc Nguyen <ngminhduc20111997@gmail.com>
2018-08-13 17:06:29 -0700

    four_point function + readme

--- 4-point-transform/fourPointTransform.py
+++ 4-point-transform/fourPointTransform.py
@@ -2,23 +2,33 @@
 import numpy as np
 import cv2
 
-# input: pts, a list of four points along with their corresponding (x,y) coordinates of the target rectangle
+# INPUT: pts, a list of four points along with their corresponding (x,y) coordinates of the target rectangle
+# OUTPUT: a 2D array
 def order_points(pts):
-    # initialize a list of 4 corner points: top-left, top-right, bottom-left, bottom-right
-    rect = np.zeros((4,2), dtype = "float32") # 4 rows 2 columns empty array, data type is float32
+    # initialize a list of 4 corner points: top-left, top-right, bottom-right, bottom-left
+    rect = np.zeros((4,2), dtype = "float32") # Empty array that has 4 rows, each row has a pair of coordinates, data type is float32
 
     # Sum along the axis = 1:
-    # Eg: pts is a LIST contains four points: [[0,0] [10,0] [10,10] [0,10]] => sum: [0, 10, 20, 10]
+    # Eg: pts is a LIST contains four points: [[0,0] [10,0] [10,10] [0,10]] => sum along the axis = 1: [0, 10, 20, 10]
     theSum = np.sum(pts, axis = 1)
     # set the top-left point to have the smallest sum, the bottom-right to have the greatest sum
-    rect[0] = pts[np.argmin(theSum)]
-    rect[2] = pts[np.argmax(theSum)]
+    rect[0] = pts[np.argmin(theSum)] # first row (top-left)
+    rect[2] = pts[np.argmax(theSum)] # third row (bottom-right)
 
     # Difference along the axis = 1:
+    # Eg: pts is a LIST contains four points: [[0,0] [10,0] [10,10] [0,10]] => difference along the axis = 1: [0, 10, 0, -10]
     theDif = np.diff(pts, axis = 1)
     # set the top-right point to have the smallest difference, the bottom-left to have the greatest difference
-    rect[1] = pts[np.argmin(theDif)]
-    rect[3] = pts[np.argmax(theDif)]
+    rect[1] = pts[np.argmin(theDif)] # second row (top-right)
+    rect[3] = pts[np.argmax(theDif)] # fourth row (bottom-left)
 
     # return this new array of coordinates
     return rect 
+
+def four_point_transform(image, pts):
+    # get a consistent order of the points and unpack them individually
+    rect = order_points(pts)
+    # top-left, top-right, bottom-right, bottom-left
+    (tl, tr, br, bl) = rect
+
+    # compute the width of the new image, which will be the maximum distance between bottom-right and bottom-left x-coordinates or top-right and top-left x-coordinates
