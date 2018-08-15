dfd28b211100c4f715dcf1015a2772c04bf4e993
Duc Nguyen <ngminhduc20111997@gmail.com>
2018-08-14 20:30:32 -0700

    finish execute

--- 4-point-transform/fourPointExecute.py
+++ 4-point-transform/fourPointExecute.py
@@ -7,3 +7,16 @@ import cv2
 
 # construct the argument parse and parse the argumnents
 ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help = "path to the input image file")
+ap.add_argument("-c", "--coords", help = "comma seperated list of source points")
+args = vars(ap.parse_args())
+
+# load the input image and get the list of source points
+image = cv2.imread(args["image"]) 
+pts = np.array(eval(args["coords"]), dtype = "float32")
+
+warped_image = four_point_transform(image, pts)
+
+cv2.imshow("Original picture", image)
+cv2.imshow("Front view of the picture", warped_image)
+cv2.waitKey(0)
