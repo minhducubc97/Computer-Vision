e6b6ad4a48f62f3879e2fe0e8e84e82aee62a218
Duc Nguyen <ngminhduc20111997@gmail.com>
2018-08-14 17:07:19 -0700

    start Executing

---
+++ 4-point-transform/fourPointExecute.py
@@ -0,0 +1,9 @@
+# import the necessary packages
+# import the helper function from the file
+from fourPointTransform import four_point_transform
+import numpy as numpy
+import argparse
+import cv2
+
+# construct the argument parse and parse the argumnents
+ap = argparse.ArgumentParser()
--- 4-point-transform/fourPointTransform.py
+++ 4-point-transform/fourPointTransform.py
@@ -28,7 +28,33 @@ def order_points(pts):
 def four_point_transform(image, pts):
     # get a consistent order of the points and unpack them individually
     rect = order_points(pts)
-    # top-left, top-right, bottom-right, bottom-left
-    (tl, tr, br, bl) = rect
+    (topLeftPoint, topRightPoint, bottomRightPoint, bottomLeftPoint) = rect
 
-    # compute the width of the new image, which will be the maximum distance between bottom-right and bottom-left x-coordinates or top-right and top-left x-coordinates
+    # calculate the width of the new image, which will be the maximum distance between top-right and top-left x-coordinates or bottom-right and bottom-left x-coordinates
+    topWidth = np.sqrt(((topRightPoint[0] - topLeftPoint[0]) ** 2) + ((topLeftPoint[1] - topRightPoint[1]) ** 2))
+    bottomWidth = np.sqrt(((bottomRightPoint[0] - bottomLeftPoint[0]) ** 2) + ((bottomLeftPoint[1] - bottomRightPoint[1]) ** 2))
+    
+    # choose the max width between the 2 calculated above to go with
+    maxWidth = max(int(topWidth), int(bottomWidth))
+
+    # calculate the height of the new image, which will be the maximum distance between top-right and bottom-right y-coordinates or top-left and bottom-left y-coordinates
+    leftHeight = np.sqrt(((topLeftPoint[0] - bottomLeftPoint[0]) ** 2) + ((topLeftPoint[1] - bottomLeftPoint[1]) ** 2))
+    rightHeight = np.sqrt(((topRightPoint[0] - bottomRightPoint[0]) ** 2) + ((topRightPoint[1] - bottomRightPoint[1]) ** 2))
+
+    # choose the max height between the 2 calculated above to go with
+    maxHeight = max(int(leftHeight), int(rightHeight))
+
+    # create the destination points (order: top-left, top-right, bottom-right and bottom-left) to obtain a front view of the image
+    dst = np.array(
+        [[0,0],
+        [maxWidth - 1, 0]
+        [maxWidth - 1, maxHeight - 1]
+        [0, maxHeight - 1]],
+        dtype = "float32")
+
+    # compute the actual perspective transform matrix and apply it
+    PTMatrix = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, PTMatrix, (maxWidth, maxHeight))
+
+    # return the warped image
+    return warped
