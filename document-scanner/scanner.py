d201a1df6feb89cbbe2092e768099eafb8da87a8
Duc Nguyen <ngminhduc20111997@gmail.com>
2018-08-16 15:53:01 -0700

    fixing scanner

--- document-scanner/scanner.py
+++ document-scanner/scanner.py
@@ -12,7 +12,50 @@ ap.add_argument("-i", "--image", required="True", help = "path to the input imag
 args = vars(ap.parse_args())
 
 # load the image, clone it and resize appropriately
-image = cv2.imread(args["image"])
-ratio = image.shape[0] / 500.0 # image.shape return (rows, columns, channels)
-cloneImage = image.copy()
+originalImage = cv2.imread(args["image"])
+ratio = originalImage.shape[0] / 500.0 # image.shape return (rows, columns, channels)
+cloneImage = originalImage.copy()
 image = imutils.resize(cloneImage, height = 500)
+
+# convert the image to grayscale in order to find contours in step 2
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# filter out weak intensity points by blurring it using GaussianBlur and then select points that are within limited intensity range
+blurred = cv2.GaussianBlur(gray, (5,5), 0)
+edged = cv2.Canny(blurred, 75, 200) # find edges that have intensity between 75 and 200
+
+# show the original image and the edge detected image
+print("STEP 1: Edge Detection")
+
+# for viewing the original image, it is necessary to resize the original image since it can be too large
+originalImageResized = imutils.resize(originalImage, height = 500) 
+cv2.imshow("Original image", originalImageResized)
+cv2.imshow("Edged image", edged)
+cv2.waitKey(0) # wait infinitely, until key event
+cv2.destroyAllWindows()
+
+# find the contours in the edged image, keeping only the one with the largest perimeter
+listOfContours = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE) # RETR_LIST: retrieve mode - in a list, CHAIN_APPROX_SIMPLE: remove all redundant points and compresses the contour
+
+# for cv2, the largest contours are stored at index 0; for cv3, the largest contours are at index 1
+largestLOC = listOfContours[0] if imutils.is_cv2() else listOfContours[1]
+# sort the list according contour area from the largest to the smallest
+largestLOC = sorted(largestLOC, key = cv2.contourArea, reverse = True)[:5]
+
+for contour in largestLOC:
+    # approximate the shape of the contour (to be a rectangle); the accuracy is chosen 2% in this case.
+    perimeter = cv2.arcLength(contour, True)
+    approxShape = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
+    # if the approximated contour has 4 points (4 corners) => safely say that we found our contour
+    if len(approxShape) == 4:
+        highlightImage = approxShape
+        break
+
+# display the image with contour highlighted
+print("STEP 2: Find contours of paper")
+cv2.drawContours(originalImage, [highlightImage], -1, (0,255, 0), 2)
+originalImageResized = imutils.resize(originalImage, height = 500)
+cv2.imshow("Highlighted contour image", originalImageResized)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+    
